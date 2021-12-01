fd70454cd954d31ac9705c74567f9217d8f0a5b5
nareshrazu <50515116+nareshrazu@users.noreply.github.com>
2021-12-01 13:55:18 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+let express = require('express');
+let app = express();
+let mongoU = 'mongodb+srv://naresh:raju1998@cluster0.xr9q3.mongodb.net/mydb?retryWrites=true&w=majority';
+let mongodb = require('mongodb');
+let MongoClient = mongodb.MongoClient;
+let db;
+let port = 8979;
+
+app.get('/',(req,res)=>{
+    db.collection('city').find({}).toArray((err,rst)=>{
+        if(err) throw err;
+        res.send(rst);
+    })
+})
+
+
+
+MongoClient.connect(mongoU,(err,connectfromurl)=>{
+    if(err) throw err;
+    db = connectfromurl.db('mydb');
+    app.listen(port, (err)=>{
+        if(err) throw err;
+    })
+        
+})
