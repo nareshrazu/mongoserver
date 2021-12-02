bd2bd24fd231cffc4ca489097234c77f28c27318
nareshrazu <50515116+nareshrazu@users.noreply.github.com>
2021-12-02 08:56:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const port = process.env.SERVER_PORT || 8000;
 
 // Add Access Control Allow Origin headers
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://nareshrazu.herokuapp.com/");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000/");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
